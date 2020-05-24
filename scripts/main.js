c1c6bd02222071e51c691b8a7a4c954dd681e36a
Sergey Vishnevsky <sergey.vishnevsky@intexsoft.by>
2020-05-24 16:36:08 +0400

    page navigation changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,36 @@ $(document).ready(function () {
   $('.scroll-to-block').on('click', (event) => {
     $('html, body').animate({
       scrollTop: $('.block-headers-about').offset().top
-    }, 2000);
+    }, 1800);
   });
 
-  $('.scroll-to-block').on('click', (event) => {
+  $('.navigation-list-link-about').on('click', (event) => {
     $('html, body').animate({
-      scrollTop: $('.block-headers-about-us').offset().top
+      scrollTop: $('.block-headers-about').offset().top
+    }, 1800);
+  });
+  $('.navigation-list-link-service').on('click', (event) => {
+    $('html, body').animate({
+      scrollTop: $('.block-headers-service').offset().top
+    }, 2000);
+  });
+  $('.navigation-list-link-trucks').on('click', (event) => {
+    $('html, body').animate({
+      scrollTop: $('.block-headers-trucks').offset().top
+    }, 2200);
+  });
+  $('.navigation-list-link-contacts').on('click', (event) => {
+    $('html, body').animate({
+      scrollTop: $('.block-headers-contacts').offset().top
+    }, 2400);
+  });
+  $('.scroll-to-top').on('click', (event) => {
+    $('html, body').animate({
+      scrollTop: $('.header-wrapper').offset().top
     }, 2000);
   });
 
-
+  $(document).on('click', 'ul li', function() {
+    $(this).addClass('navigation-list-item-active').siblings().removeClass('navigation-list-item-active')
+  });
 });
