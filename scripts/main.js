8e77d0fe199f8a8f519944dc9b64a69e5e613bb7
Sergey Vishnevsky <sergey.vishnevsky@intexsoft.by>
2020-05-24 18:55:14 +0400

    add burger, media

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,52 @@
 $(document).ready(function () {
 
+  let navActive = $('.navigation').hasClass( "active" );
+
   $('.scroll-to-block').on('click', (event) => {
     $('html, body').animate({
       scrollTop: $('.block-headers-about').offset().top
     }, 1800);
   });
+  $('.navigation-list-link-main').on('click', (event) => {
+    $('.navigation').toggleClass('active');
+    $('.burger-icon-item').toggleClass('rotate');
+    $('html, body').animate({
+    scrollTop: $('.header-wrapper').offset().top
+    }, 2200);
+  });
 
   $('.navigation-list-link-about').on('click', (event) => {
+    $('.navigation').toggleClass('active');
+    $('.burger-icon-item').toggleClass('rotate');
     $('html, body').animate({
       scrollTop: $('.block-headers-about').offset().top
     }, 1800);
   });
+
   $('.navigation-list-link-service').on('click', (event) => {
+    $('.navigation').toggleClass('active');
+    $('.burger-icon-item').toggleClass('rotate');
     $('html, body').animate({
       scrollTop: $('.block-headers-service').offset().top
     }, 2000);
   });
+
   $('.navigation-list-link-trucks').on('click', (event) => {
+    $('.navigation').toggleClass('active');
+    $('.burger-icon-item').toggleClass('rotate');
     $('html, body').animate({
       scrollTop: $('.block-headers-trucks').offset().top
     }, 2200);
   });
+
   $('.navigation-list-link-contacts').on('click', (event) => {
+    $('.navigation').toggleClass('active');
+    $('.burger-icon-item').toggleClass('rotate');
     $('html, body').animate({
       scrollTop: $('.block-headers-contacts').offset().top
     }, 2400);
   });
+
   $('.scroll-to-top').on('click', (event) => {
     $('html, body').animate({
       scrollTop: $('.header-wrapper').offset().top
@@ -35,4 +56,10 @@ $(document).ready(function () {
   $(document).on('click', 'ul li', function() {
     $(this).addClass('navigation-list-item-active').siblings().removeClass('navigation-list-item-active')
   });
+
+  $('#burger').on('click',() => {
+    $('.navigation').toggleClass('active');
+    $('.burger-icon-item').toggleClass('rotate');
+  });
+
 });
