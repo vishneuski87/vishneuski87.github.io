aa11aed7513a18f07622ea6740ed39d715c9cda7
Sergey Vishnevsky <sergey.vishnevsky@intexsoft.by>
2020-05-25 00:25:38 +0400

    add other color scheme, icons

--- scripts/main.js
+++ scripts/main.js
@@ -1,63 +1,80 @@
 $(document).ready(function () {
-
-  let navActive = $('.navigation').hasClass( "active" );
-
   $('.scroll-to-block').on('click', (event) => {
     $('html, body').animate({
       scrollTop: $('.block-headers-about').offset().top
-    }, 1800);
+    }, 2000);
   });
   $('.navigation-list-link-main').on('click', (event) => {
-    $('.navigation').toggleClass('active');
-    $('.burger-icon-item').toggleClass('rotate');
+    let windowWidth = $(window).width();
+    let navActive = ($('.navigation').hasClass("active"));
+    if (navActive && windowWidth <= 800) {
+      $('.navigation').toggleClass('active');
+      $('.burger-icon-item').toggleClass('rotate');
+    }
     $('html, body').animate({
-    scrollTop: $('.header-wrapper').offset().top
-    }, 2200);
+      scrollTop: $('.header-wrapper').offset().top
+    }, 'fast');
   });
 
   $('.navigation-list-link-about').on('click', (event) => {
-    $('.navigation').toggleClass('active');
-    $('.burger-icon-item').toggleClass('rotate');
+    let windowWidth = $(window).width();
+    let navActive = ($('.navigation').hasClass("active"));
+    if (navActive && windowWidth <= 800) {
+      $('.navigation').toggleClass('active');
+      $('.burger-icon-item').toggleClass('rotate');
+    }
     $('html, body').animate({
       scrollTop: $('.block-headers-about').offset().top
-    }, 1800);
+    }, 'fast');
   });
 
   $('.navigation-list-link-service').on('click', (event) => {
-    $('.navigation').toggleClass('active');
-    $('.burger-icon-item').toggleClass('rotate');
+    let windowWidth = $(window).width();
+    let navActive = ($('.navigation').hasClass("active"));
+    if (navActive && windowWidth <= 800) {
+      $('.navigation').toggleClass('active');
+      $('.burger-icon-item').toggleClass('rotate');
+    }
     $('html, body').animate({
       scrollTop: $('.block-headers-service').offset().top
-    }, 2000);
+    }, 'fast');
   });
 
   $('.navigation-list-link-trucks').on('click', (event) => {
-    $('.navigation').toggleClass('active');
-    $('.burger-icon-item').toggleClass('rotate');
+    let windowWidth = $(window).width();
+    let navActive = ($('.navigation').hasClass("active"));
+    if (navActive && windowWidth <= 800) {
+      $('.navigation').toggleClass('active');
+      $('.burger-icon-item').toggleClass('rotate');
+    }
     $('html, body').animate({
       scrollTop: $('.block-headers-trucks').offset().top
-    }, 2200);
+    }, 'fast');
   });
 
   $('.navigation-list-link-contacts').on('click', (event) => {
-    $('.navigation').toggleClass('active');
-    $('.burger-icon-item').toggleClass('rotate');
+    let windowWidth = $(window).width();
+    let navActive = ($('.navigation').hasClass("active"));
+    if (navActive && windowWidth <= 800) {
+      $('.navigation').toggleClass('active');
+      $('.burger-icon-item').toggleClass('rotate');
+    }
     $('html, body').animate({
       scrollTop: $('.block-headers-contacts').offset().top
-    }, 2400);
+    }, 'fast');
   });
 
   $('.scroll-to-top').on('click', (event) => {
     $('html, body').animate({
       scrollTop: $('.header-wrapper').offset().top
-    }, 2000);
+    }, 'fast');
   });
 
-  $(document).on('click', 'ul li', function() {
+  $(document).on('click', 'ul li', function () {
     $(this).addClass('navigation-list-item-active').siblings().removeClass('navigation-list-item-active')
   });
 
-  $('#burger').on('click',() => {
+  $('#burger').on('click', () => {
     $('.navigation').toggleClass('active');
     $('.burger-icon-item').toggleClass('rotate');
   });
